3d98a18bdd038da9a30041df531dc7b54cf2f3db
madurangaPrabhath <maduracourse@gmail.com>
2025-10-14 19:13:56 +0530

    added more text and paragraph

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -19,6 +20,14 @@ const Hero = () => {
           <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
             Dynamic Web Magic with Next.js
           </h2>
+
+          <TextGenerateEffect 
+            words="Transforming Ideas into Interactive Realities"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+          <p className='mb-4 text-sm md:text-lg lg:text-2xl text-center md:tracking-wider'>
+            Hi, I'm Maduranga, a passionate web developer.
+          </p>
         </div>
       </div>
     </div>
