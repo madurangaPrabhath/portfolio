7aa71fc1bf99e0a0db332f908e1ca6b82be2720e
madurangaPrabhath <maduracourse@gmail.com>
2025-10-15 13:35:32 +0530

    refactor Hero component for improved readability and update text content

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Spotlight } from './ui/Spotlight'
-import { TextGenerateEffect } from './ui/TextGenerateEffect'
-import MagicButton from './ui/MagicButton'
-import { FaLocationArrow } from 'react-icons/fa'
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <div className="pb-20 pt-36">
       <div>
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="top-10 left-full h-[80vh] w-[50vw]"
+          fill="purple"
+        />
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
@@ -17,31 +23,32 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100" />
       </div>
 
-      <div className='flex justify-center relative my-20 z-10'>
-        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-          <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
+      <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </h2>
 
-          <TextGenerateEffect 
-            words="Transforming Ideas into Interactive Realities"
+          <TextGenerateEffect
+            words="Solving Real Problems with Elegant, Human Centered Designs"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
-          <p className='mb-4 text-sm md:text-lg lg:text-2xl text-center md:tracking-wider'>
-            Hi, I'm Maduranga, a passionate web developer.
+
+          <p className="mt-6 mb-4 text-sm md:text-lg lg:text-2xl text-center md:tracking-wider">
+            Hi, I'm Maduranga, a passionate QA & web developer.
           </p>
 
-          <a href='#about'>
-            <MagicButton 
+          <a href="#about">
+            <MagicButton
               title="show my work"
               icon={<FaLocationArrow />}
-              position='right'
+              position="right"
             />
           </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
