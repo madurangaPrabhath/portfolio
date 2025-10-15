9dcf75c9eed8a612369b0f6b8412cf136c2b85c6
madurangaPrabhath <maduracourse@gmail.com>
2025-10-15 16:02:14 +0530

    added FloatingNav and Grid components, updated Home layout

---
+++ data/index.ts
@@ -0,0 +1,5 @@
+export const gridItems = [
+  { title: "Title 1", description: "Desc1", id: 1 },
+  { title: "Title 2", description: "Desc2", id: 2 },
+  { title: "Title 3", description: "Desc3", id: 3 },
+];
