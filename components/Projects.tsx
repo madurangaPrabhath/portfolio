caec7096f6459d1539dd0400d91fb0967b0a03e0
madurangaPrabhath <maduracourse@gmail.com>
2025-10-19 20:02:24 +0530

    refactor Projects component to enhance structure and display project listings

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { projects } from "@/data";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="py-20">
+      <h1 className="heading">
+        A small selection of{" "}
+        <span className="text-purple">My Projects</span>
+      </h1>
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map((item) => (
+          <div
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            key={item.id}
+          >
 
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Projects;
