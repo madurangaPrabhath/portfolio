84933abacc0b9073d80fe7e4d5efee674732faca
madurangaPrabhath <maduracourse@gmail.com>
2025-10-25 01:12:59 +0530

    added favicon.svg and update metadata for improved site branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Maduranga | Web Developer & QA Engineer",
   description: "Modern Portfolio Website",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
