4cb77ca94278ccce5e4eaf366f164f300c73d85c
madurangaPrabhath <maduracourse@gmail.com>
2025-10-24 18:13:03 +0530

    refactor code structure for improved readability and maintainability

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -61,13 +61,6 @@ const Projects = () => {
                     </div>
                   ))}
                 </div>
-
-                <div className="flex justify-center items-center">
-                  <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
-                  </p>
-                  <FaLocationArrow className="ms-3" color="#CBACF9" />
-                </div>
               </div>
             </PinContainer>
           </div>
